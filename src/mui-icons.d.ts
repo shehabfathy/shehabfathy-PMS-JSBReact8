27fe089d1bb4f910159ac539dc5ef2060d881094
shehabfathy <shehabfathy99@gmail.com>
2025-09-16 22:30:34 +0300

    Fix MUI icons TypeScript errors and prepare build

--- src/mui-icons.d.ts
+++ src/mui-icons.d.ts
@@ -1,4 +1,5 @@
-declare module "@mui/icons-material/MoreVert";
-declare module "@mui/icons-material/Visibility";
-declare module "@mui/icons-material/Delete";
-declare module "@mui/icons-material/Edit";
+declare module "@mui/icons-material/*" {
+  import { SvgIconComponent } from "@mui/material/SvgIcon";
+  const value: SvgIconComponent;
+  export default value;
+}

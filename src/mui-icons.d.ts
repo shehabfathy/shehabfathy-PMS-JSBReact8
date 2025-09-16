7b0d693ee208f2c4a04e64cd91d760977dc851b5
shehabfathy <shehabfathy99@gmail.com>
2025-09-16 08:32:56 +0300

    update pacakage json

---
+++ src/mui-icons.d.ts
@@ -0,0 +1,4 @@
+declare module "@mui/icons-material/MoreVert";
+declare module "@mui/icons-material/Visibility";
+declare module "@mui/icons-material/Delete";
+declare module "@mui/icons-material/Edit";
